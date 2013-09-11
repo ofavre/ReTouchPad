6bc6b1f286f0b688dcfdba1819c067b579edc84c
Olivier Favre <of.olivier.favre@gmail.com>
2013-09-12 00:40:47 +0200

    Try socket.io

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var sys = require("sys");
-var ws = require("websocket-server");
+var ws = require("socket.io");
 var connect = require("connect");
 var exec = require("child_process").exec;
 
@@ -14,23 +14,25 @@ function notice(data){
   sys.log("\033[0;33m"+data+"\033[0m");
 }
 
-// Fix needed when not run through `npm start`
-process.env.npm_package_config_ws_port = process.env.npm_package_config_ws_port || 3400;
-process.env.npm_package_config_web_port = process.env.npm_package_config_web_port || 8080;
+// Gather config from env
+// (default values are needed when not run through `npm start`)
+config = {
+  ws_port: parseInt(process.env.npm_package_config_ws_port || 3400),
+  web_port: parseInt(process.env.npm_package_config_web_port || 8080),
+};
 
 // HTTP Web Server used to serve the static client files
 var webserver = connect()
   .use(connect.static('www'))
-  .listen(process.env.npm_package_config_web_port);
-sys.log("Server ready to serve client files listening at port " + process.env.npm_package_config_web_port);
+  .listen(config.web_port);
+sys.log("Server ready to serve client files listening at port " + config.web_port);
 
-var server = ws.createServer();//{debug:true});
-server.listen(process.env.npm_package_config_ws_port);
-sys.log("Server ready to execute WebSocket commands listening at port " + process.env.npm_package_config_ws_port);
+var server = ws.listen(config.ws_port);
+sys.log("Server ready to execute WebSocket commands listening at port " + config.ws_port);
 
-server.addListener("connection", function(connection){
+server.sockets.on("connection", function(connection){
   notice("connection");
-  connection.addListener("message", function(msgstr){
+  connection.on("message", function(msgstr){
     var buttonToNum = { 'left': 1, 'middle': 2, 'right': 3 };
     var scrollAxisToNum = { 'y': [4,5], 'x': [6,7] };
     var zoomToNum = [4,5];
@@ -78,7 +80,7 @@ server.addListener("connection", function(connection){
       child.stdin.write('keyup Ctrl\n');
     }
   });
-  connection.addListener("close", function(){
+  connection.on("close", function(){
     notice("close");
   });
 });
