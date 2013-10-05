76a9848f24752d61e52f92cee61c9ffdc7da8ca7
Olivier Favre <of.olivier.favre@gmail.com>
2013-10-05 19:10:18 +0200

    Revert "Try engine.io", performance dropped
    This reverts commit db946af3da6a82f730d197c30816e1d65ec38866.

--- server.js
+++ server.js
@@ -18,20 +18,19 @@ function notice(data){
 process.env.npm_package_config_ws_port = process.env.npm_package_config_ws_port || 3400;
 process.env.npm_package_config_web_port = process.env.npm_package_config_web_port || 8080;
 
-process.env.DEBUG = "engine:socket,engine:ws";
-
 // HTTP Web Server used to serve the static client files
 var webserver = connect()
   .use(connect.static('www'))
   .listen(process.env.npm_package_config_web_port);
 sys.log("Server ready to serve client files listening at port " + process.env.npm_package_config_web_port);
 
-var server = require('engine.io').listen(process.env.npm_package_config_ws_port);
+var server = ws.createServer();//{debug:true});
+server.listen(process.env.npm_package_config_ws_port);
 sys.log("Server ready to execute WebSocket commands listening at port " + process.env.npm_package_config_ws_port);
 
-server.on("connection", function(connection){
+server.addListener("connection", function(connection){
   notice("connection");
-  connection.on("message", function(msgstr){
+  connection.addListener("message", function(msgstr){
     var buttonToNum = { 'left': 1, 'middle': 2, 'right': 3 };
     var scrollAxisToNum = { 'y': [4,5], 'x': [6,7] };
     var zoomToNum = [4,5];
@@ -79,7 +78,7 @@ server.on("connection", function(connection){
       child.stdin.write('keyup Ctrl\n');
     }
   });
-  connection.on("close", function(){
+  connection.addListener("close", function(){
     notice("close");
   });
 });
